b7ee4c871a15da988420574559939f99a5e4935f
amate <frandre.test@gmail.com>
2018-08-05 04:02:50 +0900

    ・旧APIへのアクセスが拒否されるようになったので削除

--- project/SeLiap/Publicity.cs
+++ project/SeLiap/Publicity.cs
@@ -81,71 +81,6 @@ namespace SeLiap
             publicitys.Clear();
 
             GetStatus_2017_1212(vid);
-            GetStatus_2017_1212_Befor(vid);
-        }
-
-        protected void GetStatus_2017_1212_Befor( string vid)
-        {
-            // 2017.12/12以前の情報を受け取る
-            var offset = 0;
-            var page_limit = 100;
-            while (true)
-            {
-                var content = GetJSONPDataByWebAPI_old(vid, offset, page_limit);
-                var ja = new JSONPAnalyze();
-                var jsonp = ja.Analyze(content);
-
-                try
-                {
-                    dynamic jsonp_meta = jsonp.value["meta"].value;
-
-                    if ((jsonp_meta["status"].value == 200) && (jsonp_meta["message"].value == "succeed"))
-                    {
-                        log.WriteLine("データ取得成功");
-
-                    }
-                    else
-                    {
-                        log.WriteLine("データ取得失敗 A");
-                        return;
-                    }
-
-
-                }
-                catch
-                {
-                    log.WriteLine("データ取得失敗 B");
-                    return;
-                }
-
-                try
-                {
-                    dynamic jsonp_data = jsonp.value["data"].value;
-                    log.WriteLine("データ数 " + jsonp_data.Count);
-                    if (jsonp_data.Count == 1)
-                    {
-                        if (jsonp_data[0].value == null)
-                        { // 中身がないのでキャンセル
-                            return;
-                        }
-                    }
-
-                    foreach (var j in jsonp_data)
-                    {
-                        var name = j.value["name"].value;
-                        var comment = j.value["campaignname"].value;
-                        var item = new PublicityData(name, comment);
-                        none_effect_publicitys.Add(item);
-                    }
-                }
-                catch
-                {
-                    return;
-                }
-
-                offset += page_limit;
-                MyLauncher.WaitSleep.Do(10);
-            }
         }
 
         protected void GetStatus_2017_1212(string vid)
@@ -207,24 +142,6 @@ namespace SeLiap
                 //if ((count_max > 0) && (count_max > offset)) break;
                 MyLauncher.WaitSleep.Do(10);
             }
-        }
-
-        protected string GetJSONPDataByWebAPI_old( string vid, int offset, int page_limit)
-        {
-            var url = string.Format(@"http://uad-api.nicovideo.jp/UadsCampaignService/getAdHistoryJsonp?vid={0}&offset={1}&limit={2}", vid, offset, page_limit);
-            log.WriteLine(url);
-
-            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
-            req.Method = "GET";
-            //req.Method = "PUSH";
-
-            HttpWebResponse res = (HttpWebResponse)req.GetResponse();
-
-            Stream s = res.GetResponseStream();
-            StreamReader sr = new StreamReader(s);
-            string content = sr.ReadToEnd();
-
-            return content;
         }
 
         // 取得方法が異なるので
